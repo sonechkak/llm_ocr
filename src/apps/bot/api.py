e41f5f9f2cf8ff73d3789b757fc6e92160637ebf
sonya <sonyakarm@icloud.com>
2025-07-01 18:27:28 +0300

    fixed fincs

--- src/apps/bot/api.py
+++ src/apps/bot/api.py
@@ -5,8 +5,9 @@ from .services.summarizer import summarize_document
 
 bot_router = APIRouter()
 
+
 @bot_router.post("/summarize", status_code=status.HTTP_201_CREATED)
-async def bot_result(file: UploadFile = File(...)):
+async def summarize_file(file: UploadFile = File(...)):
     """Summarizes the uploaded document."""
     try:
         summary = await summarize_document(file)
